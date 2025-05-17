fab530acbd973729891631b5088913889acb23e4
QuaffMedia <quaffmedia@gmail.com>
2025-05-17 20:53:19 +0530

    Updated Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -241,17 +241,7 @@ const Services: React.FC<ServicesProps> = ({ serviceSlug }) => {
     </div>
     
     {/* Call to action - medium size */}
-    <div className="text-center mt-6">
-      <Link 
-        to="/contact" 
-        className="inline-flex items-center bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2.5 px-7 rounded-md text-base transition-colors duration-300"
-      >
-        Schedule a Consultation
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
-        </svg>
-      </Link>
-    </div>
+
   </div>
 </section>
         
