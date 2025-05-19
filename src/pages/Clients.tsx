9c14d8d6640800daef4defa6baf40b9d65bc8459
QuaffMedia <quaffmedia@gmail.com>
2025-05-19 20:31:08 +0530

    Updated Clients.tsx

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -128,7 +128,7 @@ const Clients: React.FC = () => {
       </p>
     </div>
 
-    <div className="flex space-x-6 overflow-x-auto animate-left scrollbar-hide">
+    <div className="flex space-x-6 overflow-x-auto animate-slide-left scrollbar-hide">
       {[
         {
           name: "Indian Oil Corporation",
