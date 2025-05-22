386730ebdd464178ee98b82500c81980812d3a64
QuaffMedia <quaffmedia@gmail.com>
2025-05-22 12:29:06 +0530

    Updated ContactForm.tsx

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -51,7 +51,6 @@ const ContactForm: React.FC = () => {
 >
   <input type="hidden" name="_captcha" value="false" />
   <input type="hidden" name="_subject" value="New Contact Submission - TrustAudit" />
-  <input type="hidden" name="_next" value="https://taggifyaudit.com/thank-you" />
 <input type="hidden" name="_from" value="TrustAudit Website" />
 
       <div className="mb-8">
