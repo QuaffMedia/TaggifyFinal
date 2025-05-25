e2996148567b09d8e551771eccb72fa9ceb9fa1b
QuaffMedia <quaffmedia@gmail.com>
2025-05-25 23:42:53 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import Router from './components/Router';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Industries from './pages/Industries';
+import Process from './pages/Process';
+import Clients from './pages/Clients';
+import Resources from './pages/Resources';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Router />
-    </div>
+    <>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/services/:serviceSlug" element={<Services />} />
+          <Route path="/industries" element={<Industries />} />
+          <Route path="/process" element={<Process />} />
+          <Route path="/clients" element={<Clients />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
