4da6babe2f980936814409a961009ed2d6fc4710
QuaffMedia <quaffmedia@gmail.com>
2025-05-26 02:24:10 +0530

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Building, Mail, Phone, MapPin, Linkedin, Twitter, Facebook } from 'lucide-react';
-import { Link } from './Link';
+import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => {
   return (
