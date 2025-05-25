0296e4ffd7dcf279f30772ebe76593b9154bfbe5
QuaffMedia <quaffmedia@gmail.com>
2025-05-26 02:27:31 +0530

    Updated ServiceCard.tsx

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ArrowRight } from 'lucide-react';
-import { Link } from './Link';
+import { Link } from 'react-router-dom';
+
 
 interface ServiceCardProps {
   icon: React.ReactNode;
