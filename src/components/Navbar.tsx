465b45c786646451198f77bd2e0d0fa189b08c42
QuaffMedia <quaffmedia@gmail.com>
2025-05-27 00:48:31 +0530

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -73,10 +73,11 @@ const Navbar: React.FC = () => {
               </button>
               {showServicesDropdown && (
                 <div className="absolute bg-white shadow-lg rounded-md mt-2 py-2 w-48">
+                  <Link to="/services/call-auditing" className="block px-4 py-2 text-gray-800 hover:bg-blue-50">Call Auditing</Link>
                   <Link to="/services/bpo" className="block px-4 py-2 text-gray-800 hover:bg-blue-50">BPO Audits</Link>
                   <Link to="/services/kpo" className="block px-4 py-2 text-gray-800 hover:bg-blue-50">KPO Compliance</Link>
                   <Link to="/services/social-media" className="block px-4 py-2 text-gray-800 hover:bg-blue-50">Social Media Audits</Link>
-                  <Link to="/services/call-auditing" className="block px-4 py-2 text-gray-800 hover:bg-blue-50">Call Auditing</Link>
+                  
 <Link to="/services/data-security" className="block px-4 py-2 text-gray-800 hover:bg-blue-50">Data Security</Link>
 
                 </div>
